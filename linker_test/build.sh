932ac9f23d348e235410343f065ae7f0b09b7a0a
sa-kei728 <keisuke.sato1@konicaminolta.com>
2021-08-23 18:44:06 +0900

    miss script

--- linker_test/build.sh
+++ linker_test/build.sh
@@ -4,4 +4,4 @@
 g++ -shared -fPIC -o libfunc1.so func1.cpp -Wl,-rpath=../linker_test_sub -L../linker_test_sub/ -lfunc2
 
 # currentdirであるlibfunc2.soのrpathを相対パスで埋め込んだtest.elfの生成
-g++ main.cpp -Wl,-rpath=./linker_test -L./ -lfunc1 -o test.elf
+g++ main.cpp -Wl,-rpath=./ -L./ -lfunc1 -o test.elf
